d574892ac255271bb92b6f13398d5ee395ddaa50
Hoang bieu <bieu.hv@gmail.com>
2014-03-19 16:28:25 +0700

    FF

--- wp-config.php
+++ wp-config.php
@@ -79,7 +79,13 @@ define('WPLANG', '');
  * in their development environments.
  */
 define('WP_DEBUG', false);
-
+define( 'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'localhost');
+define('PATH_CURRENT_SITE', '/lacviet/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
